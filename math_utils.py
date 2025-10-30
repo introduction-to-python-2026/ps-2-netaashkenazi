6ecc1643c36206edbbd92272fe691c7d4ae16c7e
netaashkenazi <neta.ashkenazi.email@gmail.com>
2025-10-30 21:32:51 +0200

    Update math_utils.py

--- math_utils.py
+++ math_utils.py
@@ -7,11 +7,10 @@ def find_max_number(num1, num2, num3):
         return num3
 
 def find_mean(num1, num2, num3):
-    sum = num1 + num2 + num3
-    mean = sum/n
+    mean = (num1 + num2 + num3)/3
     return mean
 
 def find_mean_std(num1, num2, num3):
     mean = find_mean(num1, num2, num3)
-    st = (((num1 - mean)**2 + (num2 - mean)**2 + (num3 - mean)**2)/n)**0.5
+    st = (((num1 - mean)**2 + (num2 - mean)**2 + (num3 - mean)**2)/3)**0.5
     return st
