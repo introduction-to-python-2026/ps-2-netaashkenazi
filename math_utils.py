b1d953acd85e0aa9dd7fb478489aa25327e99df0
netaashkenazi <neta.ashkenazi.email@gmail.com>
2025-10-30 21:41:38 +0200

    Update math_utils.py

--- math_utils.py
+++ math_utils.py
@@ -12,5 +12,5 @@ def find_mean(num1, num2, num3):
 
 def find_mean_std(num1, num2, num3):
     mean = find_mean(num1, num2, num3)
-    st = (((num1 - mean)**2 + (num2 - mean)**2 + (num3 - mean)**2)/3)**0.5
-    return st
+    std = (((num1 - mean)**2 + (num2 - mean)**2 + (num3 - mean)**2)/3)**0.5
+    return mean, std
